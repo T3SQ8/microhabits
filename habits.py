4f89c72f759ee900e3e9fda1f5e93002ce714858
T3SQ8 <h9s@tutanota.com>
2022-09-23 00:10:25 +0200

    Added functionality + refactoring
    - Default files in XDG_DATA_HOME/microhabits and
      XDG_CONFIG_HOME/microhabits
    - Command-line flags to set log and habits files
    - Dropped Habits class in favour of plain functions
    - Added message types
    - Misc refactoring
    - `example.yml` merged into script (file created if it doesn't exist)

--- habits.py
+++ habits.py
@@ -1,78 +1,100 @@
 #!/usr/bin/env python
 
 import sys
-from os.path import exists
+import os
 from datetime import datetime, timedelta
 import csv
 import curses
 import re
 import yaml
 
+FIELDNAMES = ['date', 'name', 'status']
+
+YAML_EXAMPLE_DATA = """
+habits:
+    - name: Exercise
+      frequency: ['Monday', 'Wednesday', 'Saturday'] # Specific days of the week
+
+    - name: Clean House
+      frequency: 3 # Every 3 days
+
+    - name: Backup
+      frequency: ['1st', '15th'] # Every 1st and 15th of every month
+
+    - name: Take a long walk on the beach # Example of long name
+      # Default frequency daily
+
+    - name: Play videogames # Habits that don't need to be done but are tracked
+      frequency: 0
+"""
+
 DAYS_BACK = 3
 DAYS_FORWARD = 1
 
-class Habits:
-    def __init__(self, habits_file, log_file):
-        self.fieldnames = ['date', 'name', 'status']
-        self.habits_file = habits_file
-        self.log_file = log_file
-        self.log = {}
-        self.habits = {}
-        if not exists(self.habits_file):
-            print(f"No such file or directory: '{self.habits_file}'")
-            sys.exit(1)
-        if not exists(self.log_file):
-            with open(self.log_file, 'w', encoding='utf-8') as file:
-                csv.writer(file).writerow(self.fieldnames)
+def load_habits_from_file(habits_file):
+    if not os.path.exists(habits_file):
+        with open(habits_file, 'w', encoding='utf-8') as file:
+            file.write(YAML_EXAMPLE_DATA)
+    with open(habits_file, 'r', encoding='utf-8') as file:
+        habits = yaml.safe_load(file)['habits']
+    for habit in habits: # Default frequency daily
+        if not 'frequency' in habit:
+            habit['frequency'] = 1
 
-    def load_habits_from_file(self):
-        with open(self.habits_file, 'r', encoding='utf-8') as file:
-            habits = yaml.safe_load(file)
-            self.habits = habits['habits']
+    return habits
 
-        for habit in self.habits: # Default frequency daily
-            if not 'frequency' in habit:
-                habit['frequency'] = 1
-
-    def load_log_from_file(self):
-        log = []
-        self.log = {}
-        with open(self.log_file, 'r', encoding='utf-8') as file:
-            for row in csv.DictReader(file):
-                log.append(row)
-        # Creates a 'log' dictionary, inside of each entry is another dictionary with a date and a
-        # corresponding status. This process removes duplicate CSV entries by overwriting the date
-        # with the newest value.
-        for _dict in log:
-            try:
-                self.log[_dict['name']]
-            except KeyError:
-                self.log[_dict['name']] = {}
-            self.log[_dict['name']][_dict['date']] = _dict['status']
-
-    def dump_log_to_file(self):
-        log = []
-        # As a result of the way the log is processed, the entries are grouped together by the
-        # habit.
-        for habit, dates in self.log.items():
-            for date, status in dates.items():
-                log.append({
-                        'date': date,
-                        'name': habit,
-                        'status': status
-                })
-        with open(self.log_file, 'w', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=self.fieldnames)
+def load_log_from_file(log_file):
+    if not os.path.exists(log_file):
+        with open(log_file, 'w', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
             writer.writeheader()
-            for _dict in log:
-                writer.writerow(_dict)
+    log = {}
+    with open(log_file, 'r', encoding='utf-8') as file:
+        for line in csv.DictReader(file):
+            # Creates a 'log' dictionary, inside of each entry is another dictionary with dates and
+            # a corresponding etatuses. This process removes duplicate CSV entries by overwriting
+            # the date with the newest value.
+            try:
+                log[line['name']]
+            except KeyError:
+                log[line['name']] = {}
+            name = line['name']
+            date = line['date']
+            status = line['status']
+            log[name][date] = status
+    return log
 
-    def curses_tui(self):
-        curses.wrapper(self.curses_loop)
+def dump_log_to_file(log, log_file):
+    dump = []
+    for habit, dates in log.items():
+        for date, status in dates.items():
+            dump.append({
+                    'date': date,
+                    'name': habit,
+                    'status': status
+            })
+    with open(log_file, 'w', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
+        writer.writeheader()
+        for line in dump:
+            writer.writerow(line)
 
-    def gen_content(self, start_day, before_days, forward_days):
+def curses_tui(habits, log, log_file):
+    def message(y_max, x_max, stdscr, msg, mode=None):
+        match mode:
+            case 'prompt':
+                pos = 2
+            case _:
+                pos = 1
+        msg = msg.ljust(x_max-1) # Pad the message so that it covers any previous message
+        stdscr.addstr(y_max-pos, 0, msg)
 
-        before_days = -before_days if before_days > 0 else before_days # Make negative if positive
+    def save():
+        dump_log_to_file(log, log_file)
+        #message(f'Saved to {log_file}')
+
+    def gen_content(start_day, before_days, forward_days):
+        before_days = -abs(before_days)
 
         header = []
         header.append(' '.ljust(66) + '-----------')
@@ -86,7 +108,7 @@ class Habits:
         header.append(day_header)
 
         habits_list = []
-        for habit in self.habits:
+        for habit in habits:
             row = ''
             name = habit['name'][:22]+'>' if len(habit['name']) > 22 else habit['name']
             row += name.ljust(24)
@@ -95,9 +117,9 @@ class Habits:
                 screen_date = start_day + timedelta(days=i)
                 screen_date = screen_date.strftime('%Y-%m-%d')
                 try:
-                    textbox = f'[{self.log[habit["name"]][screen_date]}]'
+                    textbox = f'[{log[habit["name"]][screen_date]}]'
                 except KeyError:
-                    if self.check_due(habit, screen_date):
+                    if check_due(habit, screen_date):
                         textbox = '[ ]'
                     else:
                         textbox = '[o]'
@@ -107,21 +129,21 @@ class Habits:
 
         return header, habits_list
 
-    def toggle_status(self, habit_name, date):
-        if not habit_name in self.log:
-            self.log[habit_name] = {}
-        if not date in self.log[habit_name]:
-            self.log[habit_name][date] = {}
+    def toggle_status(habit_name, date):
+        if not habit_name in log:
+            log[habit_name] = {}
+        if not date in log[habit_name]:
+            log[habit_name][date] = {}
 
-        match self.log[habit_name][date]:
+        match log[habit_name][date]:
             case 'y':
-                self.log[habit_name][date] = 's'
+                log[habit_name][date] = 's'
             case 's':
-                del self.log[habit_name][date]
+                del log[habit_name][date]
             case _:
-                self.log[habit_name][date] = 'y'
+                log[habit_name][date] = 'y'
 
-    def check_due(self, habit, selected_date):
+    def check_due(habit, selected_date):
         due = True
         frequency = habit['frequency']
         name = habit['name']
@@ -132,11 +154,11 @@ class Habits:
                 if frequency == 0:
                     due = False
                 try:
-                    for status_date in sorted(self.log[name]):
-                        status = self.log[name][status_date]
+                    for status_date in sorted(log[name]):
+                        status = log[name][status_date]
                         if status == 'y':
                             tmp_last_done = datetime.strptime(status_date, "%Y-%m-%d")
-                            if not tmp_last_done.date() > selected_date.date():
+                            if tmp_last_done.date() < selected_date.date():
                                 last_done = tmp_last_done
                 except KeyError:
                     pass
@@ -153,40 +175,34 @@ class Habits:
                     selected_date = selected_date.strftime('%-d') # non-padded day of month
                     if not selected_date in frequency:
                         due = False
-                except Exception:
+                except AttributeError:
                     frequency = [ item.capitalize() for item in frequency ]
                     selected_date = selected_date.strftime('%A') # Day of week
                     if not selected_date.capitalize() in frequency:
                         due = False
         return due
 
-    def curses_loop(self, stdscr):
-        y_max, x_max = stdscr.getmaxyx()
+    def curses_loop(stdscr):
         now = datetime.today()
         col = 0
         row = 0
         current_row = 0
         curses.curs_set(0)
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-
-        def message(msg):
-            msg = msg.ljust(x_max-1) # Pad the message so that it covers any previous message
-            stdscr.addstr(y_max-1, 0, msg)
-
-        def save():
-            self.dump_log_to_file()
-            message(f'Saved to {self.log_file}')
-
         while True:
+            y_max, x_max = stdscr.getmaxyx()
             stdscr.erase()
             message(
-                    'keys: k/UP,j/DOWN:Select habit   h/LEFT,l/RIGHT:Select day   SPACE/RETURN:Toggle status   t:Jump to today   q:Save and exit   Q:Exit'
+                    y_max,
+                    x_max,
+                    stdscr,
+                    'keys: k/UP,j/DOWN:Select habit   h/LEFT,l/RIGHT:Select day   SPACE/RETURN:Toggle status   t:Jump to today   q:Save and exit   Q:Exit',
                     )
-            header, habits = self.gen_content(now, DAYS_BACK, DAYS_FORWARD)
+            header, menu_habits = gen_content(now, DAYS_BACK, DAYS_FORWARD)
             for row, line in enumerate(header):
                 stdscr.addstr(row, 0, line)
                 row += 1
-            for idy, text in enumerate(habits):
+            for idy, text in enumerate(menu_habits):
                 if idy == current_row:
                     stdscr.attron(curses.color_pair(1))
                     stdscr.addstr(row, col, text)
@@ -197,15 +213,15 @@ class Habits:
             key = stdscr.getch()
             if (key == curses.KEY_UP or key == ord('k')) and current_row > 0:
                 current_row -= 1
-            if (key == curses.KEY_DOWN  or key == ord('j')) and current_row < len(habits) - 1:
+            if (key == curses.KEY_DOWN  or key == ord('j')) and current_row < len(menu_habits) - 1:
                 current_row += 1
             if key == curses.KEY_LEFT or key == ord('h'):
                 now = now - timedelta(days=1)
             if key == curses.KEY_RIGHT or key == ord('l'):
                 now = now + timedelta(days=1)
             if key == curses.KEY_ENTER or key in [ord('\n'), ord('\r'), ord(' ')]:
-                self.toggle_status(
-                    self.habits[current_row]['name'],
+                toggle_status(
+                    habits[current_row]['name'],
                     now.strftime('%Y-%m-%d')
                     )
             if key == ord('t'):
@@ -217,22 +233,48 @@ class Habits:
                 save()
                 sys.exit(1)
             if key == ord('Q'):
-                message('Do you want to quit without saving? (N/y)')
+                message(
+                        y_max,
+                        x_max,
+                        stdscr,
+                        'Do you want to quit without saving? (N/y)',
+                        'prompt'
+                        )
                 key = stdscr.getch()
                 if key == ord('y') or key == ord('Y'):
                     sys.exit(1)
 
-def main(habits_file):
-    log_file = habits_file[:habits_file.rfind(".")] + '.csv'
-    habits = Habits(habits_file, log_file)
-    habits.load_habits_from_file()
-    habits.load_log_from_file()
-    habits.curses_tui()
+    curses.wrapper(curses_loop)
+
+def main(habits_file, log_file):
+    if not habits_file:
+        try:
+            habits_file = os.environ['XDG_CONFIG_HOME'] + '/microhabits/habits.yml'
+        except KeyError:
+            habits_file = os.environ['HOME'] + '/.config/microhabits/habits.yml'
+
+    if not log_file:
+        try:
+            log_file = os.environ['XDG_DATA_HOME'] + '/microhabits/log.csv'
+        except KeyError:
+            log_file = os.environ['HOME'] + '/.config/microhabits/log.csv'
+
+    for path in [habits_file, log_file]:
+        parents = os.path.dirname(path)
+        if parents:
+            os.makedirs(parents, exist_ok=True)
+
+    habits = load_habits_from_file(habits_file)
+    log = load_log_from_file(log_file)
+    curses_tui(habits, log, log_file)
 
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(description='Visually plot ledger files.')
-    parser.add_argument('habits_file', metavar='FILE', help='Habits file in YAML format')
+    parser = argparse.ArgumentParser(description='Minimalistic habit tracker')
+    parser.add_argument('-f', '--file', dest='habits_file', metavar='FILE',
+            help='Habits file in YAML format')
+    parser.add_argument('-l', '--log_file',
+            help='File to log activity to')
     args = parser.parse_args()
-    main(args.habits_file)
+    main(args.habits_file, args.log_file)
